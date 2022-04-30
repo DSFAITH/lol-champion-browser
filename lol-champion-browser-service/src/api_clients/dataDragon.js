d33ee39da398decf7f04adb0c52f322da3dc24a7
Dani Faith <dani.crader@gmail.com>
2022-04-30 15:21:48 -0500

    commit to draven

--- lol-champion-browser-service/src/api_clients/dataDragon.js
+++ lol-champion-browser-service/src/api_clients/dataDragon.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 
 const DD_REGION = 'na'
-let version = undefined
+let fetchedVersion = undefined
 let realmData = undefined
 let championData = undefined
 
@@ -12,16 +12,22 @@ export async function getChampionData() {
     }
 
     realmData = await getRealmData()
-    version = realmData.v
+    fetchedVersion = realmData.v
 
     const championURL = `${realmData.cdn}/${realmData.n.champion}/data/${realmData.l}/champion.json`
     const championResponse = await fetch(championURL)
     championData = await championResponse.json()
 
+    deriveSquareImageURLs(Object.values(championData.data), realmData.cdn, fetchedVersion)
+
     return championData
 }
 
 async function getRealmData() {
-    const realmResponse = await fetch(`https://ddragon.leagueoflegends.com/realms/$DD_REGION}.json`)
+    const realmResponse = await fetch(`https://ddragon.leagueoflegends.com/realms/${DD_REGION}.json`)
     return await realmResponse.json()
+}
+
+function deriveSquareImageURLs(champions, baseURL, version) {
+    champions.forEach(champion => champion.squareImageURL = `${baseURL}/${version}/img/champion/${champion.id}.png`)
 }
--- lol-champion-browser-service/src/routes/v1/champions/championsRoutes.js
+++ lol-champion-browser-service/src/routes/v1/champions/championsRoutes.js
@@ -16,19 +16,33 @@ function registerChampionsRoutes() {
 async function handleGetChampions(request, response) {
 
     const championData = await getChampionData()
-    response.json(championData)
+    const transformed = transformChampions(Object.values(championData.data))
+    response.json(transformed)
 }
 
 async function handleGetChampion(request, response) {
-    const championName = request.params.championData
+    const championID = request.params.champion
     const championData = await getChampionData()
 
-    const champion = championData.data[championName]
+    const champion = championData.data[championID]
     if (!champion) {
-        response.status(404).send({error: `champion ${championName} not found`})
+        response.status(404).send({error: `champion ${championID} not found`})
     } else {
-        response.json(champion)
+        response.json(transformChampion(champion))
     }
+}
+
+function transformChampions(champions) {
+    return champions.reduce((transformed, champion) => {
+        transformed.push(transformChampion(champion))
+        return transformed
+    }, [])
+}
+
+function transformChampion(champion) {
+    const { id, name, title, blurb, tags, squareImageURL } = champion
+    return { id, name, title, blurb, tags, squareImageURL }
+
 }
 
 export { registerChampionsRoutes }
