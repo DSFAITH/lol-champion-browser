45b2c254546b1b5c7f93010113766dac95c2693d
Dani Faith <dani.crader@gmail.com>
2022-07-25 18:46:28 -0500

    commit on gwen

--- lol-champion-browser-ui/src/feature/champions/list/component.js
+++ lol-champion-browser-ui/src/feature/champions/list/component.js
@@ -1,13 +1,24 @@
 import './style.css'
+import { useState, useEffect } from 'react'
+import { getChampions } from '../../../services/champions'
 
-export function ChampionsList(props) {
-    const{ champions } = props
+export function ChampionsList() {
+    const[champions, setChampions] = useState([])
+    useEffect( () => {
+        async function fetchChampions() {
+            const fetchedChampions = await getChampions()
+            setChampions(fetchedChampions)
+    }
+    fetchChampions()
+}, [])
     return (
 
         <div className="container">
+            {}
             <Header className="Header"/>
             <div className="ChampionsList">
-                { champions.map(champion => (<ChampionPanel champion={champion}/>))}
+                {}
+                { champions.map(champion => (<ChampionPanel key={champion.id}champion={champion} />))}
             </div>
 
         </div>
@@ -15,12 +26,18 @@ export function ChampionsList(props) {
 
 
 }
-function ChampionPanel() {
+function ChampionPanel(props) {
+    const { champion } = props
     return (
-        <div className="ChampionPanel">
-            <div className="image"/>
-            <div className="caption"/>
-        </div>
+        <div className="ChampionPanel"
+           onMouseOver={event => setHighlighted(event.target)}
+           onMouseOut={event => unsetHighlighted(event.target)}>
+            <img src={champion.loadingImageURL} className="image" alt={champion.name}/>
+            <div className="caption">
+                <span>{champion.name}</span>
+            </div>
+            
+           </div>
     )
 }
 function Header() {
@@ -28,3 +45,17 @@ function Header() {
         <div className="Header"/>
     )
 }
+function setHighlighted(element) {
+    const panel = element.parentNode
+    panel.childNodes.forEach(child => {
+      child.classList.remove('unhighlight')
+      child.classList.add('highlight')
+    })
+  }
+  function unsetHighlighted(element) {
+    const panel = element.parentNode
+    panel.childNodes.forEach(child => {
+      child.classList.add('unhighlight')
+      child.classList.remove('highlight')
+    })
+  }
